c6c315c2e053d487a4bd6d62b3111d84d25eca1e
Laura Aguilar <laura.aguilarv1@gmail.com>
2019-07-08 23:59:58 -0500

    details

--- src/app/places/places.page.spec.ts
+++ src/app/places/places.page.spec.ts
@@ -1,23 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { PlacesPage } from './places.page';
 
-
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('PlacesPage', () => {
+  let component: PlacesPage;
+  let fixture: ComponentFixture<PlacesPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ PlacesPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(PlacesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/places/places.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-places',
+  templateUrl: './places.page.html',
+  styleUrls: ['./places.page.scss'],
+})
+export class PlacesPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  go(){
+    this.router.navigate(['details']);
+  }
+
+  ngOnInit() {
+  }
+
+}
